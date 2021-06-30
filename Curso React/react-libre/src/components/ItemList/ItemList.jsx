b5601bf077bbe93b9aa2c37841d86c386de137b3
KevinAndresSegovia <kevinandressegovia@gmail.com>
2021-06-30 14:29:08 -0300

    Clase 6. Funciona todo

--- Curso React/react-libre/src/components/ItemList/ItemList.jsx
+++ Curso React/react-libre/src/components/ItemList/ItemList.jsx
@@ -1,63 +1,40 @@
-import React, { Fragment } from "react";
-import Item from "../Item/Item";
+import React, { Fragment, useEffect, useState } from "react";
+//import Item from "../Item/Item";
+import { getData } from "../../utils/utils";
+import CardComponent from "../CardComponent/CardComponent";
 
 export default function ItemList() {
-  const product = [
-    {
-      id: 0,
-      name: "Moto e",
-      img: "https://cdn.pixabay.com/photo/2016/10/12/16/32/smartphone-1735044_960_720.jpg",
-      descripcion:
-        "Android... Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-      price: 50,
-      stock: 4,
-      comprar: 0,
-    },
-    {
-      id: 1,
-      name: "Moto g",
-      img: "https://cdn.pixabay.com/photo/2016/10/12/16/32/smartphone-1735044_960_720.jpg",
-      descripcion:
-        "Android... Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-      price: 80,
-      stock: 0,
-      comprar: 0,
-    },
-    {
-      id: 2,
-      name: "Moto z",
-      img: "https://cdn.pixabay.com/photo/2016/10/12/16/32/smartphone-1735044_960_720.jpg",
-      descripcion:
-        "Android... Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-      price: 30,
-      stock: 8,
-      comprar: 0,
-    },
-    {
-      id: 3,
-      name: "Moto x",
-      img: "https://cdn.pixabay.com/photo/2016/10/12/16/32/smartphone-1735044_960_720.jpg",
-      descripcion:
-        "Android... Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-      price: 90,
-      stock: 5,
-      comprar: 0,
-    },
-  ];
+  let [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const waitForData = async () => {
+      let data = await getData("smartphones");
+      let aux = data.map((element) => {
+        return {
+          name: element.title,
+          img: element.thumbnail,
+          price: element.price,
+          stock: element.available_quantity,
+        };
+      });
+      setProducts(aux);
+    };
+
+    waitForData();
+  }, []);
 
   return (
     <Fragment>
-      {product.map((element) => {
+      {products.map((element, index) => {
         return (
-          (
-            <Item
+          <Fragment key={index}>
+            <CardComponent
               name={element.name}
               img={element.img}
-              descripcion={element.descripcion}
               price={element.price}
               stock={element.stock}
             />
-          )
+          </Fragment>
         );
       })}
     </Fragment>
--- Curso React/react-libre/src/containers/ItemListContainer.jsx
+++ Curso React/react-libre/src/containers/ItemListContainer.jsx
@@ -2,27 +2,9 @@ import React, { Fragment } from "react";
 import ItemList from "../components/ItemList/ItemList";
 
 export default function ItemListContainer() {
-  /* const getProduct = () => {
-  return new Promise ((resolve, reject) => {
-    setTimeout(() => {
-      resolve(lista)
-    }, 2000)
-
-  })
-}
-async function mostrarProductos (){
-  const product = await getProduct()
-  console.log(product)
-}
-
-
-mostrarProductos() */
-
-
-
   return (
     <Fragment>
-      <ItemList />;
+      <ItemList />
     </Fragment>
   );
 }
